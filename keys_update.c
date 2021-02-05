deef25f64459896aae2fef6f8a73d02a01e8fbf7
Mohammed ali Kellati <mokellat@e1r8p12.1337.ma>
2021-02-05 16:44:43 +0100

    a and d fixed

--- keys_update.c
+++ keys_update.c
@@ -12,6 +12,10 @@ int		key_pressed(int key)
 		g_new_player.turn_direction = -1;
 	if(key == RIGHT)
 		g_new_player.turn_direction = 1;
+	if(key == A)
+		g_new_player.side_direction = 1;
+	if(key == D)
+		g_new_player.side_direction = -1;
 	return 0;
 }
 
@@ -25,6 +29,10 @@ int		key_released(int key)
 		g_new_player.turn_direction = 0;
 	if(key == RIGHT)
 		g_new_player.turn_direction = 0;
+	if(key == A)
+		g_new_player.side_direction = 0;
+	if(key == D)
+		g_new_player.side_direction = 0;
 	return 0;
 }
 
@@ -34,21 +42,39 @@ int		red_cross(void)
 	exit(EXIT_SUCCESS);
 }
 
+void	player_move(double angle, double move_step, double move_speed)
+{
+	if (g_new_player.side_direction != 0)
+    {
+		angle = g_new_player.rotation_angle - M_PI / 2;
+		move_step = g_new_player.side_direction * move_speed;
+		g_new_player.new_pos_x = g_new_player.pos_x + cos(angle) * move_step;
+		g_new_player.new_pos_y = g_new_player.pos_y + sin(angle) * move_step;
+    }
+	else
+	{
+		g_new_player.rotation_angle += g_new_player.turn_direction * g_new_player.rotation_speed;
+		move_step = g_new_player.walk_direction * move_speed;
+		g_new_player.new_pos_x = g_new_player.pos_x + cos(g_new_player.rotation_angle) * move_step;
+		g_new_player.new_pos_y = g_new_player.pos_y + sin(g_new_player.rotation_angle) * move_step;
+	}
+}
+
 int		update()
 {
 	double	move_step;
 	double	move_speed;
 	int		k;
+	double	angle;
 
 	move_speed = 10.0;
+	angle = 0.0;
+	move_step = 0.0;
 	mlx_clear_window(g_mlx.mlx_ptr, g_mlx.win_ptr);
 	mlx_destroy_image(g_mlx.mlx_ptr, g_mlx.img_ptr);
 	g_mlx.img_ptr = mlx_new_image(g_mlx.mlx_ptr, g_width, g_height);
 	g_mlx.img_data = (int *)mlx_get_data_addr(g_mlx.img_ptr, &k, &k, &k);
-	g_new_player.rotation_angle += g_new_player.turn_direction * g_new_player.rotation_speed;
-	move_step = g_new_player.walk_direction * move_speed;
-	g_new_player.new_pos_x = g_new_player.pos_x + cos(g_new_player.rotation_angle) * move_step;
-	g_new_player.new_pos_y = g_new_player.pos_y + sin(g_new_player.rotation_angle) * move_step;
+	player_move(angle, move_step, move_speed);
 	if (!haswallat(g_new_player.new_pos_x , g_new_player.new_pos_y))
 	{
 		g_new_player.pos_x = g_new_player.new_pos_x;
